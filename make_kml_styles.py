69467b5a0e07e37302b86b94e448e679d4d86337
schwehr <schwehr@a19cddd1-5311-0410-bb07-9ca93daf0f0b>
2009-07-04 18:19:11 +0000

    silly little program to start with random colors
    git-svn-id: https://cowfish.unh.edu/projects/schwehr/trunk/src/ais-areanotice-py@12219 a19cddd1-5311-0410-bb07-9ca93daf0f0b

---
+++ make_kml_styles.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# Generate random colors and styles for all the areas
+
+from imo_001_22_area_notice import notice_type
+import random
+
+def hex_len2(h):
+    'make sure the hex numbers are len 2'
+    if len(h) == 2:
+        return h
+    return '0'+h
+
+def rand_hex_color():
+    return ''.join( [ hex_len2(hex(int(random.random()*255)).split('x')[1]) for j in range(3) ])
+
+for i in range(3,127+1):
+    
+    print '''
+	<!-- AreaNotice {number}: {name} -->
+	<StyleMap id="AreaNotice_{number}">
+		<Pair><key>normal</key><styleUrl>#AreaNotice_{number}_normal</styleUrl></Pair>
+		<Pair><key>highlight</key><styleUrl>#AreaNotice_{number}_highlight</styleUrl></Pair>
+	</StyleMap>
+
+	<Style id="AreaNotice_{number}_highlight">
+          <IconStyle><scale>1.2</scale></IconStyle>
+	  <LineStyle><color>{c1}</color><width>8</width></LineStyle>
+	  <PolyStyle><color>{c2}</color>
+        </Style>
+	<Style id="AreaNotice_{number}_normal">
+	  <LineStyle><color>{c3}</color><width>8</width></LineStyle>
+	  <PolyStyle><color>{c4}</color></PolyStyle>
+	</Style>'''.format(number=i, name=notice_type[i], 
+                           c1=rand_hex_color(),
+                           c2=rand_hex_color(),
+                           c3=rand_hex_color(),
+                           c4=rand_hex_color())
+
