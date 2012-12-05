eab3bcf08ac840837a75075f6e0458cbf0119e89
Roland Arsenault <roland@rolker.net>
2012-12-05 12:30:17 -0800

    Factored out header checks Implemented more of circle checks, but not yet complete Added stub for rectangle check

--- test_m367_22.py
+++ test_m367_22.py
@@ -6,11 +6,31 @@ from m367_22 import AreaNotice
 
 class TestAreaNotice(unittest.TestCase):
 
+    def checkHeader(self, area_notice, mmsi=366123456):
+        self.assertEqual(area_notice.message_id, 8)
+        self.assertEqual(area_notice.repeat_indicator, 0)
+        self.assertEqual(area_notice.source_mmsi, mmsi)
+        self.assertEqual(area_notice.spare, 0)
+        
     def testCircle(self):
         msg = '!AIVDM,1,1,0,A,85M:Ih1KmPAU6jAs85`03cJm;1NHQhPFP000,0*19'
         area_notice = AreaNotice(nmea_strings=[msg])
-        self.assertEqual(area_notice.message_id, 8)
-        self.assertEqual(area_notice.repeat_indicator, 0)
+        self.checkHeader(area_notice)
+        self.assertEqual(area_notice.dac, 367)      # USA
+        self.assertEqual(area_notice.fi, 22)        # area notice
+        self.assertEqual(area_notice.version, 1)
+        self.assertEqual(area_notice.link_id, 101)
+        self.assertEqual(area_notice.area_type, 13) # Caution Area: Survey Operations
+        #timestamp = datetime.datetime(
+        #self.assertEqual(area_notice.when,
+
+    def testRectangle(self):
+        msg = '!AIVDM,1,1,0,A,85M:Ih1KmPAVhjAs80e0;cKBN1N:W8Q@:2`0,0*0C'
+        area_notice = AreaNotice(nmea_strings=[msg])
+        self.checkHeader(area_notice)
+
+        
+        
 
 if __name__ == '__main__':
     unittest.main()
